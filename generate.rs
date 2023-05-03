af6d5456b691ca6328f29da8042bf749463cb9a5
ozwaldorf <self@ossian.dev>
2023-05-03 03:04:41 -0400

    refactor: use /tmp for palettes

--- generate.rs
+++ generate.rs
@@ -3,6 +3,7 @@
 
 use std::{
     io::{stdout, Write},
+    path::PathBuf,
     process::Command,
 };
 
@@ -13,8 +14,6 @@ use image::{
 };
 
 fn main() {
-    // Ensure directories exist
-    std::fs::create_dir("./palettes").ok();
     std::fs::create_dir("./src").ok();
 
     println!("Generating catppuccin LUTs: ");
@@ -25,21 +24,23 @@ fn main() {
         print!("{name} ...");
         stdout().flush().ok();
 
-        let colors: Vec<Colour> = flavor.colours().into_iter().collect();
-        let len = colors.len() as u32;
-        let mut palette = ImageBuffer::new(len, 1);
+        let palette_path = &format!("/tmp/{name}.png");
+        if !PathBuf::from(palette_path).exists() {
+            let colors: Vec<Colour> = flavor.colours().into_iter().collect();
+            let len = colors.len() as u32;
+            let mut palette = ImageBuffer::new(len, 1);
 
-        // Place the colors one after another in the buffer
-        for (pixel, color) in palette.pixels_mut().zip(colors.into_iter()) {
-            let (r, g, b) = color.into();
-            *pixel = Rgb([r, g, b]);
+            // Place the colors one after another in the buffer
+            for (pixel, color) in palette.pixels_mut().zip(colors.into_iter()) {
+                let (r, g, b) = color.into();
+                *pixel = Rgb([r, g, b]);
+            }
+
+            // Resize the palette for convenience
+            resize(&palette, len * 20, 20, FilterType::Nearest)
+                .save(palette_path)
+                .expect("error saving pallet image");
         }
-
-        // Resize the palette for convenience
-        let palette_path = &format!("palettes/{name}.png");
-        resize(&palette, len * 20, 20, FilterType::Nearest)
-            .save(palette_path)
-            .expect("error saving pallet image");
 
         // Generate the LUT from the pallet image
         let output = Command::new("sh")
