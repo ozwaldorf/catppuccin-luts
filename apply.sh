f8c60e09b7fa9759d5e834dcc0cefb08827a36b7
ozwaldorf <self@ossian.dev>
2023-05-05 08:19:51 -0400

    feat: example apply.sh script

---
+++ apply.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+src="$(dirname "$(realpath -s "$0")")/src"
+
+print_usage() {
+  name=`basename $0`
+  cat <<EOF
+Usage: $name -i <image1> ... [args]
+    -i image  :  Image to apply LUT to, can be repeated
+    -f flavor :  Flavor to apply, can be repeated. Defaults to all
+    -n noise  :  LUT Noise level (1-4). Defaults to 2
+EOF
+}
+
+noise="2"
+images=""
+flavors=""
+
+while getopts ':f:i:n:' flag; do
+  case "$flag" in
+    n)
+      if [[ $OPTARG -gt 4 ]]; then
+        echo "Invalid noise level (0-4)"
+        exit 1
+      else
+        noise=$OPTARG
+      fi
+      ;;
+    f) flavors="$flavors $OPTARG" ;;
+    i) images="$images `realpath $OPTARG`" ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+if [[ "$images" == "" ]]; then
+  print_usage
+fi
+
+if [[ "$flavors" == "" ]]; then
+  flavors="oled mocha macchiato frappe latte"
+fi
+
+for file in $images; do
+  echo "Performing magick on `basename $file` (noise: $noise)"
+  for flavor in $flavors; do
+    printf "$flavor ..."
+    out="${file%%.*}-${flavor}.png"
+    magick $file "$src/noise_$noise/${flavor}.png" -hald-clut $out
+    printf "\r$flavor ✓ ($out)\n"
+  done
+done
+
